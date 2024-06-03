ee8e60ef8d8ff018438c3f94c7c1ccb590a9dfb7
JoseACP <jose.chairez.is@unipolidgo.edu.mx>
2024-06-03 15:47:20 -0600

    readme_up

--- tasks/views.py
+++ tasks/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
-def helloworld(request):
-    title = 'Hello world'
-    return render(request, 'signup.html', {
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    return render(request, 'signup.html',{
         'form': UserCreationForm
     })
--- tienda/urls.py
+++ tienda/urls.py
@@ -20,5 +20,7 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.helloworld),
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('home/', views.home, name='home'),  # Asegúrate de que esta línea esté presente
 ]
